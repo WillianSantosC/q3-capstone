f6cdbcf89cc6dfadbfa80c1e73a3ab8989e432a8
Willian Santos <williansantosd2@gmail.com>
2022-03-03 20:52:41 -0300

    feat: patch route added for group

--- app/models/group_model.py
+++ app/models/group_model.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from uuid import uuid4
 
-from sqlalchemy import Boolean, Column, String, ForeignKey
+from sqlalchemy import Boolean, Column, ForeignKey, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
