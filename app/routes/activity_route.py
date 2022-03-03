b301d72a843eedd7310d15a0dfa76deb26a71d07
WillianSantosC <85417990+WillianSantosC@users.noreply.github.com>
2022-03-03 11:30:26 -0300

    Delete app directory

--- app/routes/activity_route.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-from app.controllers.activity_post_controller import (activity_post,
-                                                      activity_post_time)
-
-bp = Blueprint('bp_activity', __name__, url_prefix='/api/activity')
-
-bp.post('')(activity_post)
-bp.post('/time/<id>')(activity_post_time)
--- app/routes/group_route.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-from app.controllers.group_post_controller import create_group
-
-bp = Blueprint('bp', __name__, url_prefix='/api/group')
-
-bp.post('')(create_group)
