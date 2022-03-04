bf67e36ca29decbc54464799962348cbd7ccc993
Maria Rubini <maria.rubini@pravaler.com.br>
2022-03-04 20:56:23 -0300

    Added post card controller route to project

--- app/routes/card_route.py
+++ app/routes/card_route.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 
 from app.controllers.card_patch_controller import update_card
+from app.controllers.card_post_controller import create_card
 
 
 bp = Blueprint('bp_card', __name__, url_prefix='/api/card')
+bp.post('/<activity_id>')(create_card)
 bp.patch('/<id>')(update_card)
