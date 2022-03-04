a5e7c7b07931ab5758daf13e7773ec61a487b101
Natan <natanwsn@gmail.com>
2022-03-04 18:53:52 -0300

    feat: created delete comment route

--- app/routes/comment_route.py
+++ app/routes/comment_route.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from app.controllers.comment_delete_controller import delete_comment
 from app.controllers.comment_patch_controller import update_comment
 
 from app.controllers.comment_post_controller import create_comment
@@ -9,3 +10,4 @@ bp = Blueprint('bp_comment', __name__, url_prefix='/api/<group_id>/comment')
 bp.post('')(create_comment)
 bp.get('')(comment_get)
 bp.patch('/<comment_id>')(update_comment)
+bp.delete('/<comment_id>')(delete_comment)
