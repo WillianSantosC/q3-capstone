192ef82a2248546054d445dae69d41f11cd21ed5
hank <matheus_bueno10@hotmail.com>
2022-03-04 20:30:46 -0300

    feat: feito o delete do card

--- app/routes/card_route.py
+++ app/routes/card_route.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 
+from app.controllers.card_delete_controller import delete_card
 from app.controllers.card_patch_controller import update_card
 
 
 bp = Blueprint('bp_card', __name__, url_prefix='/api/card')
 bp.patch('/<id>')(update_card)
+bp.delete('/<id>')(delete_card)
