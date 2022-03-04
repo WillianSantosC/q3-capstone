be1a6fefde0b96deba0607e8514b07b411d81a67
Gabriel Gutierez <gabriel@gabriel-inspiron-14-3467>
2022-03-04 20:19:26 -0300

    feat: patch card

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,10 +4,12 @@ from app.routes.activity_route import bp as bp_activity
 from app.routes.comment_route import bp as bp_comment
 from app.routes.group_route import bp as bp_group
 from app.routes.user_route import bp as bp_user
-
+from app.routes.card_route import bp as bp_card
 
 def init_app(app: Flask):
     app.register_blueprint(bp_user)
     app.register_blueprint(bp_activity)
     app.register_blueprint(bp_group)
     app.register_blueprint(bp_comment)
+    app.register_blueprint(bp_card)
+
