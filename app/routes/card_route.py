710ece6fe719579f5b7eb18c8346c23bca1bd0eb
Maria Rubini <maria.rubini@pravaler.com.br>
2022-03-04 21:08:22 -0300

    Added get card function with route

--- app/routes/card_route.py
+++ app/routes/card_route.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from app.controllers.card_get_controller import get_card
 
 from app.controllers.card_patch_controller import update_card
 from app.controllers.card_post_controller import create_card
@@ -6,4 +7,5 @@ from app.controllers.card_post_controller import create_card
 
 bp = Blueprint('bp_card', __name__, url_prefix='/api/card')
 bp.post('/<activity_id>')(create_card)
+bp.get('<id>')(get_card)
 bp.patch('/<id>')(update_card)
